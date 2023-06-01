5ed7460793b12b5179c83e8a94ac3edd0ef67d85
pthienquan1 <quan01011997123@gmail.com>
2023-06-01 10:48:55 +0700

    Init prj, create package DataTypes_Variables and demo

---
+++ Self-Study-Kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("hê lô bờ rô")
+
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
