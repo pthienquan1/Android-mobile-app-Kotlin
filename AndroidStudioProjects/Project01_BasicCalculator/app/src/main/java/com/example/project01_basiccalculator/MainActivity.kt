8c619768d133d1a6866ed20af1cdbd673ca1b2ed
pthienquan1 <quan01011997123@gmail.com>
2023-06-15 20:58:14 +0700

    list view, save item from user input

--- AndroidStudioProjects/Project01_BasicCalculator/app/src/main/java/com/example/project01_basiccalculator/MainActivity.kt
+++ AndroidStudioProjects/Project01_BasicCalculator/app/src/main/java/com/example/project01_basiccalculator/MainActivity.kt
@@ -1,76 +1,83 @@
 package com.example.project01_basiccalculator
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.project01_basiccalculator.databinding.ActivityMainBinding
 
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    var eptLst:MutableList<String> = mutableListOf("01231043104","123134141");
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val edtTextA =  findViewById<EditText>(R.id.edtTextA);
-        val edtTextB = findViewById<EditText>(R.id.edtTextB);
-        val edtTextKQ = findViewById<EditText>(R.id.edtKQ);
-        val btnReset = findViewById<Button>(R.id.btnReset);
-        val btnCong = findViewById<Button>(R.id.btnCong);
-        val btnNhan = findViewById<Button>(R.id.btnNhan);
-        val btnChia = findViewById<Button>(R.id.btnChia);
-        val btnGiuLau = findViewById<Button>(R.id.btnGiuLau);
-        val btnFinish = findViewById<Button>(R.id.btnFinish);
 
-        btnCong.setOnClickListener{
-            val A = edtTextA.text.toString().toInt();
-            val B = edtTextB.text.toString().toInt();
-            val kq = A+B;
-            edtTextKQ.setText(kq.toString());
+        //khởi tạo view binding
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
 
-        }
+//        var colorChang = Color.parseColor("#99FFFF")
+//        binding.lvQuocGia.setBackgroundColor(colorChang);
 
-        btnReset.setOnClickListener{
-            edtTextA.setText("");
-            edtTextB.setText("");
-            edtTextKQ.setText("");
-            Toast.makeText(this,"BẠN ĐÃ RESET TẤT CẢ !",Toast.LENGTH_LONG).show();
-        }
 
-        //chia sẻ biến để dùng chung
-        var share: View.OnClickListener? =null;
-        share = View.OnClickListener {
-            if(it == btnNhan){
-                val A = edtTextA.text.toString().toInt();
-                val B = edtTextB.text.toString().toInt();
-                val kq = A*B;
-                edtTextKQ.setText(kq.toString());
-            }
-            else if(it == btnChia){
-                val A = edtTextA.text.toString().toDouble();
-                val B = edtTextB.text.toString().toDouble();
-                val kq = A/B;
-                edtTextKQ.setText(kq.toString());
+        //khai bao list rong
 
-            }
-        }
-        btnNhan.setOnClickListener(share);
-        btnChia.setOnClickListener(share);
+        binding.lvQuocGia.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,eptLst)
+        addEvents();
 
-        var NhanLau: View.OnLongClickListener? =null;
-        NhanLau = View.OnLongClickListener {
-            if(it == btnGiuLau){
-                Toast.makeText(this, "Lêu lêu có gì đâu đồ ngốc :-)", Toast.LENGTH_LONG).show();
-                btnGiuLau.visibility = View.GONE;
-            }
-            true
-        }
-        btnGiuLau.setOnLongClickListener(NhanLau);
 
-        btnFinish.setOnClickListener{
-            Toast.makeText(this,"Thiên Quân cảm ơn bạn vì đã sử dụng phần mềm",Toast.LENGTH_LONG).show();
-            finish();
+    }
+
+    private fun addEvents() {
+//        HienThiQuocGia();
+        binding.btnSave.setOnClickListener {
+            XuLySaveSDT();
         }
     }
+
+    private fun XuLySaveSDT() {
+        var s:String = binding.edtInput.text.toString();
+        eptLst.add(s);
+        binding.lvQuocGia.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,eptLst)
+        binding.edtInput.setText("")
+    }
+//
+//    private fun HienThiQuocGia() {
+//        var arrQuocGia = resources.getStringArray(R.array.arrQuocGia1)
+//        binding.lvQuocGia.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,arrQuocGia)
+//
+//        binding.lvQuocGia.setOnItemClickListener { parent, view, position, id ->
+//            Toast.makeText(this,"Ban chon quoc gia "+ arrQuocGia[position],Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
+
+    //bài về ảnh img view
+//    private fun addEvents(){
+//        binding.radPic1.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if(isChecked){
+//                binding.imgPic.setImageResource(R.drawable.pic1);
+//            }
+//        }
+//
+//        binding.radPic2.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if(isChecked){
+//                binding.imgPic.setImageResource(R.drawable.pic2);
+//            }
+//        }
+//        binding.imgExit.setOnClickListener {
+//            finish()
+//        }
+//
+//    }
+
 }
